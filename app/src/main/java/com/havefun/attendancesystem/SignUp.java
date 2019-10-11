eec32cda37b6067b119de39d895f616ac242312a
Have_Fun <havefun77612@outlook.com>
2019-10-11 11:28:14 +0200

    Enable Google Sign Auth

--- app/src/main/java/com/havefun/attendancesystem/SignUp.java
+++ app/src/main/java/com/havefun/attendancesystem/SignUp.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.FrameLayout;
@@ -14,11 +15,19 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.shashank.sony.fancytoastlib.FancyToast;
 
 import java.util.regex.Matcher;
@@ -31,6 +40,9 @@ public class SignUp extends AppCompatActivity {
     FrameLayout sginupbtn;
     FirebaseAuth mAuth;
     ProgressBar progressBar;
+    SignInButton google;
+    GoogleSignInClient mGoogleSignInClient;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -132,6 +144,80 @@ public class SignUp extends AppCompatActivity {
             return false;
         }
     }
+    /*
+        ///////// Google Sign in part
+     */
+
+
+    /*
+        +++  Google Sign in Methods
+ */
+    private void  googleSignIn() {
+
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, 101);
+    }
+    /*
+====== To Listen For teh Result OF Google Sined in User
+   */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        // Note the RequestCode Must Be The Same Asa the Google Code USed
+        if (requestCode == 101) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                if(account!=null) {
+                    firebaseAuthWithGoogle(account);
+                }
+                //   progressBar.setVisibility(View.INVISIBLE);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                FancyToast.makeText(getApplicationContext(),"Google sign in failed",FancyToast.LENGTH_LONG,FancyToast.ERROR,true).show();
+                // ...
+
+                Log.w("mMessage", "Google sign in failed", e);
+                //   progressBar.setVisibility(View.INVISIBLE);
+            }
+
+        }
+
+    }
+
+
+    /*
+     ///// Authonticate With Google
+     */
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        // Toast.makeText(this, "firebaseAuthWithGoogle:" + acct.getId(), Toast.LENGTH_SHORT).show();
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FancyToast.makeText(getApplicationContext(),"signIn succefully"
+                                    ,FancyToast.LENGTH_LONG,FancyToast.SUCCESS,true).show();
+                            user = mAuth.getCurrentUser();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            FancyToast.makeText(getApplicationContext(),"signIn failed ",FancyToast.LENGTH_LONG,FancyToast.ERROR
+                                    ,true).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+
 
     /*
     :::: Disable The Progress View
@@ -166,12 +252,24 @@ public class SignUp extends AppCompatActivity {
                 GetData();
             }
         });
+        google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                googleSignIn();
+            }
+        });
     }
 
     private void initializeVars() {
         gotologinbtn = (TextView) findViewById(R.id.gotologinbtn);
         password = (EditText) findViewById(R.id.password);
         email = (EditText) findViewById(R.id.email);
+        google=findViewById(R.id.google);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
         sginupbtn = (FrameLayout) findViewById(R.id.sginupbtn);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
         mAuth = FirebaseAuth.getInstance();
