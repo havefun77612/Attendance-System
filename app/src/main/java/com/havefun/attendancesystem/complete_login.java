9737f6943e2d1b391ada5332724993b327714f49
EslamAhmed12 <56762218+EslamAhmed12@users.noreply.github.com>
2019-10-28 16:50:29 +0200

    complete_login is added

--- app/src/main/java/com/havefun/attendancesystem/complete_login.java
+++ app/src/main/java/com/havefun/attendancesystem/complete_login.java
@@ -1,14 +1,16 @@
-package com.example.login;
+package com.havefun.attendancesystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.havefun.attendancesystem.R;
+
+public class complete_login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.complete_login);
     }
 }
