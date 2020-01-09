7c835888cbb5bad61a4395a34cf44990b93c95cb
EslamAhmed12 <56762218+EslamAhmed12@users.noreply.github.com>
2020-01-09 21:33:54 +0200

    Scan Page design and structure is added (2 fragments , the first has button to start Scanning process while the other fragment has the results of Scanning process and you can search for your result by writing your id in search area )

---
+++ app/src/main/java/com/havefun/attendancesystem/Scanning_page/Scan.java
@@ -0,0 +1,63 @@
+package com.havefun.attendancesystem.Scanning_page;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+
+import com.havefun.attendancesystem.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Scan extends Fragment {
+Button button;
+Animation animate;
+
+    public Scan() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+
+                             Bundle savedInstanceState) {
+
+
+ return inflater.inflate( R.layout.fragment_scan2, container, false);
+    }
+
+
+
+
+    /*
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(anim!=null && !anim.isRunning()){
+            anim.start();
+        }
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        if(anim!=null && anim.isRunning()){
+            anim.stop();
+        }
+    }
+*/
+}
+
+  /*
+        intVars();
+        anim = (AnimationDrawable) button.getBackground();
+        anim.setEnterFadeDuration(2300);
+        anim.setExitFadeDuration(2300);
+        */
