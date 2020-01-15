c483c1472515c6496343dcdc487be392d36c8b64
Have_Fun <havefun77612@outlook.com>
2020-01-16 00:00:18 +0200

    adding Qr Generating feature by Hussien

--- app/src/main/java/com/havefun/attendancesystem/MainPage.java
+++ app/src/main/java/com/havefun/attendancesystem/MainPage.java
@@ -19,6 +19,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.havefun.attendancesystem.Chat.MainChat;
 import com.havefun.attendancesystem.Profile.ProfileActivity;
+import com.havefun.attendancesystem.QR.QrGeneration;
 import com.havefun.attendancesystem.QR.ScanQr;
 
 
@@ -26,7 +27,7 @@ public class MainPage extends AppCompatActivity implements NavigationView.OnNavi
     Toolbar toolbar;
     DrawerLayout drawer;
     ActionBarDrawerToggle toggle;
-    LinearLayout loginactivity,profile_page,Scaner,waiting;
+    LinearLayout loginactivity,profile_page,Scaner,waiting,qrgeneration;
     FirebaseUser user;
     DBManager offlineDB;
 
@@ -67,7 +68,9 @@ private void logOut(){
         profile_page=(LinearLayout)findViewById(R.id.profile_page);
         Scaner=(LinearLayout)findViewById(R.id.Scaner);
         waiting=(LinearLayout)findViewById(R.id.waiting);
+        qrgeneration=(LinearLayout)findViewById(R.id.qrgeneration);
         toolbar =(Toolbar) findViewById(R.id.toolbar);
+
         user= FirebaseAuth.getInstance().getCurrentUser();
         offlineDB=new DBManager(getApplicationContext());
         setSupportActionBar(toolbar);
@@ -106,6 +109,12 @@ private void logOut(){
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(), MainChat.class));
 
+            }
+        });
+        qrgeneration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), QrGeneration.class));
             }
         });
 
