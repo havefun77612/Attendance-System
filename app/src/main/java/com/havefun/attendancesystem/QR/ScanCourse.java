caeb5926df118999764949c52e68b53aae939eb9
husonic <45578079+husonic@users.noreply.github.com>
2020-03-02 17:48:22 +0200

    fixed back button of cource

--- app/src/main/java/com/havefun/attendancesystem/QR/ScanCourse.java
+++ app/src/main/java/com/havefun/attendancesystem/QR/ScanCourse.java
@@ -120,7 +120,8 @@ public class ScanCourse extends AppCompatActivity implements ZXingScannerView.Re
     public void onBackPressed() {
         if (zx != null) {
             zx.removeAllViews();//here comes the crash
-            startActivity(new Intent(getApplicationContext(), ScanQr.class));
+            //startActivity(new Intent(getApplicationContext(), ScanQr.class));
+            startActivity(new Intent(getApplicationContext(), ScanCourse.class));
             finish();
 
 
